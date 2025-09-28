567858addacd3a22515b2f7c3e37a6e3c9585a34
Pristine duru <Favourduru24@gmail.com>
2025-09-27 19:28:13 -0700

    implement the search service

--- search-service/src/server.js
+++ search-service/src/server.js
@@ -1,9 +1,27 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 4000
+const cors = require('cors')
+const corsOption = require('./config/corsOption')
+const cookieParser = require('cookie-parser')
+const connectDB = require('./config/dbConnc')
+const {logger} = require('./middleware/logger')
+const errorMiddleware = require('./middleware/errorHandler')
+const PORT = process.env.PORT || 3002
+const helmet = require('helmet')
+ 
 
+//middleware
+app.use(express.json())
+app.use(express.static('public'))
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}))
+app.use(cors(corsOption))
+app.use(logger)
+app.use(helmet())
 
+ 
+  app.use(errorMiddleware)
 
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-})
+  connectDB()
+  app.listen(PORT, () => {console.log(`service: identity-service running on port ${PORT}`)})
